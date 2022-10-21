5974bc8662838877e6b04b021de2d15d6e239d02
William Overton <woverton@fastly.com>
2022-10-21 13:41:43 +0100

    update

--- NextJSContentStitching/lib/banner.js
+++ NextJSContentStitching/lib/banner.js
@@ -2,7 +2,7 @@
 export const getBannerContent = async () => {
   const f = await fetch("https://sd-origin.global.ssl.fastly.net/product/quote.html", {
     backend: "product_origin",
-    cacheOverride: new CacheOverride("pass"),
+    cacheOverride: new CacheOverride("override", { ttl: 120, surrogateKey: "banner" }),
   });
 
   const content = await f.text();
--- NextJSContentStitching/lib/products.js
+++ NextJSContentStitching/lib/products.js
@@ -2,7 +2,7 @@
  * Get product from API
  */
 export const getProducts = async () => {
-  const url = `https://sd-origin.global.ssl.fastly.net/product/range?from=0&to=10`;
+  const url = `https://sd-origin.global.ssl.fastly.net/product/range?from=0&to=4`;
 
   const response = await fetch(url, {
     backend: "product_origin",
